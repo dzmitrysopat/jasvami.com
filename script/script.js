2925b1bf991ab04a98146eeece6d31edf6d25b4f
dzmitrysopat <dzmitrysopat@gmail.com>
2023-08-09 14:05:52 +0300

    main

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const carousel = document.querySelector(".carousel");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    // prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mousedown", dragStop);
