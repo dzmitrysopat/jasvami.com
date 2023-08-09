fddc9f260261130c7c990addb28aaca470bec0d5
dzmitrysopat <dzmitrysopat@gmail.com>
2023-08-09 15:34:31 +0300

    main

--- script/script.js
+++ script/script.js
@@ -2,12 +2,12 @@ const carousel = document.querySelector(".carousel"),
 firstImg = carousel.querySelectorAll("img")[0],
 arrowIcons = document.querySelectorAll(".courses i");
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
-const showHideIcons = () => {
-    // showing and hiding prev/next icon according to carousel scroll left value
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
-    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
-}
+// const showHideIcons = () => {
+//     // showing and hiding prev/next icon according to carousel scroll left value
+//     let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+//     arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+//     arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+// }
 arrowIcons.forEach(icon => {
     icon.addEventListener("click", () => {
         let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
