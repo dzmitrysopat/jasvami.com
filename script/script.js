86b0fb0c0935481ba747aee3a52616f5a4f548cd
dzmitrysopat <dzmitrysopat@gmail.com>
2023-08-17 16:47:27 +0300

    main

--- script/script.js
+++ script/script.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
-    console.log(scrollbar);
-    document.querySelector('[href="#openModal"]').addEventListener('click',function(){
-      document.body.style.overflow = 'hidden';
-      document.querySelector('#openModal').style.marginLeft = scrollbar;
-    });
-    document.querySelector('[href="#close"]').addEventListener('click',function(){
-      document.body.style.overflow = 'visible';
-      document.querySelector('#openModal').style.marginLeft = '0px';
-    });
-  });
+var now = new Date();
+// например, выведем текущую дату в консоль
+console.log(now);
