ddb0bec459db9e47da9da67e016e521e6437a0bd
dzmitrysopat <dzmitrysopat@gmail.com>
2023-08-22 14:21:31 +0300

    main

--- script/script.js
+++ script/script.js
@@ -1,14 +1,18 @@
-const footerContainer = document.querySelector('#copyright');
+// const footerContainer = document.querySelector('#copyright');
 
-var now = new Date();
-// например, выведем текущую дату в консоль
-// console.log(now.getFullYear());
+// var now = new Date();
+// // например, выведем текущую дату в консоль
+// // console.log(now.getFullYear());
 
-function clearPage(){
-    footerContainer.innerHTML = '';
-}
+// function clearPage(){
+//     footerContainer.innerHTML = '';
+// }
 
-const CopyrightTemplate = ` <section class="copyright">Анна Манько, Психолог рядом с Вами, %year%</section>`;
-    const year = CopyrightTemplate.replace('%year%', now.getFullYear());
-    footerContainer.innerHTML = year;
-// console.log(now.getFullYear());
+// const CopyrightTemplate = ` <section class="copyright">Анна Манько, Психолог рядом с Вами, %year%</section>`;
+//     const year = CopyrightTemplate.replace('%year%', now.getFullYear());
+//     footerContainer.innerHTML = year;
+// // console.log(now.getFullYear());
+
+setTimeout(function(){
+	document.body.classList.add('body_visible');
+}, 200);
