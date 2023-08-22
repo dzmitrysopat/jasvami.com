39d5ef4b6e4eed95ee08402114519e12d40498fc
dzmitrysopat <dzmitrysopat@gmail.com>
2023-08-22 08:44:43 +0300

    main

--- script/random.js
+++ script/random.js
@@ -1,7 +1,7 @@
-let imgs = ['/img/form/consultation-green-01.png', 
-'/img/form/consultation-green-02.png', 
-'/img/form/consultation-orange-01.png', 
-'/img/form/consultation-orange-02.png']; //   Добавьте свои картинки через запятую
+let imgs = ['img/form/consultation-green-01.png', 
+'img/form/consultation-green-02.png', 
+'img/form/consultation-orange-01.png', 
+'img/form/consultation-orange-02.png']; //   Добавьте свои картинки через запятую
 let i = Math.floor(Math.random() * imgs.length);
 let not__found = document.getElementById("not__found");
 imgsrc('img/form'); // Первоначальное заполнение картинки
