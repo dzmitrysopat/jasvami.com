3b2a27f7ffc75b1325459a6f0f25c95510b00da8
dzmitrysopat <dzmitrysopat@gmail.com>
2023-08-24 14:57:15 +0300

    main

--- script/script.js
+++ script/script.js
@@ -15,4 +15,21 @@
 
 setTimeout(function(){
 	document.body.classList.add('body_visible');
-}, 200);
+}, 500);
+
+const openModalButton = document.getElementById('consultation');
+const closeModalButton = document.getElementById('close-btn');
+const modalWrapper = document.getElementById('modalWrapper');
+
+openModalButton.addEventListener('click', () =>{
+	modalWrapper.style.display = 'block';
+	document.body.style.overflow = 'hidden';
+	document.getElementById('modalWrapper').classList.add('body_visible');
+	setTimeout(function(){
+		document.getElementById('modalWrapper').classList.add('body_visible');
+	}, 500);
+});
+
+closeModalButton.addEventListener('click', () =>{
+	modalWrapper.style.display = 'none';
+});
