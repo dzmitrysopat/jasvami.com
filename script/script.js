d660d2e06ad867a777ab79633958ad176942c6dd
dzmitrysopat <dzmitrysopat@gmail.com>
2023-08-24 15:11:42 +0300

    main

--- script/script.js
+++ script/script.js
@@ -17,19 +17,29 @@ setTimeout(function(){
 	document.body.classList.add('body_visible');
 }, 500);
 
+
 const openModalButton = document.getElementById('consultation');
+const openPersonalButton = document.getElementById('personalConsultation');
+const openFamilyButton = document.getElementById('familyConsultation');
 const closeModalButton = document.getElementById('close-btn');
 const modalWrapper = document.getElementById('modalWrapper');
 
 openModalButton.addEventListener('click', () =>{
 	modalWrapper.style.display = 'block';
 	document.body.style.overflow = 'hidden';
-	document.getElementById('modalWrapper').classList.add('body_visible');
-	setTimeout(function(){
-		document.getElementById('modalWrapper').classList.add('body_visible');
-	}, 500);
+});
+
+openPersonalButton.addEventListener('click', () =>{
+	modalWrapper.style.display = 'block';
+	document.body.style.overflow = 'hidden';
+});
+
+openFamilyButton.addEventListener('click', () =>{
+	modalWrapper.style.display = 'block';
+	document.body.style.overflow = 'hidden';
 });
 
 closeModalButton.addEventListener('click', () =>{
 	modalWrapper.style.display = 'none';
+	document.body.style.overflow = 'visible';
 });
