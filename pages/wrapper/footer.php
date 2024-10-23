f9e2c5acfc9fcb47db261cb009594578da057a1f
dzmitrysopat <102973988+dzmitrysopat@users.noreply.github.com>
2024-10-24 00:17:15 +0300

    main

---
+++ pages/wrapper/footer.php
@@ -0,0 +1,80 @@
+	<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style/wrapper.css">
+    <link rel="stylesheet" href="../../style/mobile.css">
+    <title>footer</title>
+</head>
+<body>
+    <footer>
+        <article class="footer-lists">
+            <section>
+                <h3 class="list-title">Контакты:</h3>
+                <ul class="footer-list">
+		<a href="https://www.b17.ru/manko_anna/" target="_blank" class="footer-link">
+			<img src="../../img/icons/check-white.svg" alt="">
+			Анкета на b17.ru
+		</a>
+                   <a href="tel:+375296525073" class="footer-link">
+                        <img src="../../img/icons/phone-white.svg" alt="">
+                        +375 (29) 652-50-73
+                    </a>
+                    <a href="mailto:hannamanko@jasvami.com" class="footer-link">
+                        <img src="../../img/icons/mail-white.svg" alt="">hannamanko@jasvami.com          </a>
+                    <a href="https://yandex.by/maps/-/C2aDnvT" target="_blank" class="footer-link">
+                        <img src="../../img/icons/location-white.svg" alt="">
+                        246020, Беларусь, г. Гомель, ул. Сосновая,18 каб. 6.2
+                    </a>
+                </ul>
+            </section>
+
+	    <section>
+                <h3 class="list-title">Реквизиты:</h3>
+                <ul class="footer-list">
+			<a class="footer-link">
+                        <img src="../../img/icons/doc-white-footer.svg" alt="">
+                        УНП 791290674
+                    	</a>                   
+			<a href="../../docs/price.pdf" download class="footer-link">
+                        <img src="../../img/icons/doc-white-footer.svg" alt="">
+                        Прейскурант
+                    	</a>
+                    	<a href="../../docs/public_agreegment_15.10.24.pdf" download class="footer-link">
+                        <img src="../../img/icons/doc-white-footer.svg" alt="">
+                        Публичный договор
+                    	</a>
+		        </ul>
+            </section>       
+
+<section>
+                <h3 class="list-title">Мессенджеры:</h3>
+                <ul class="footer-list">
+                    <a href="https://wa.me/375296525073" class="footer-link">
+                        <img src="../../img/icons/whatsapp-white-footer.svg" alt="">
+                        Whatsapp
+                    </a>
+                    <a href="https://t.me/Hannet_M" class="footer-link">
+                        <img src="../../img/icons/telegram-white-footer.svg" alt="">
+                        Telegram
+                    </a>
+                    <a href="https://viber.click/375296525073" class="footer-link">
+                        <img src="../../img/icons/viber-white-footer.svg" alt="">
+                        Viber
+                    </a>
+                </ul>
+            </section>	        
+            </article>
+        <hr class="none">
+        <article class="copyright">
+            <a class="logotype" href="../../index.php">
+                <img class="footer-favicon" src="../../img/icons/logo-inverse.svg" alt="">
+                <h2 class="logo-title footer-title">Анна Манько <br> Психолог рядом с Вами</h2>
+            </a>
+        </article>
+        <hr class="">
+        <img src="../../img/icons/payment-icons.png" alt="" class="payment-icons">
+    </footer>
+</body>
+</html>
